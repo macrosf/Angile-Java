9dc2970485b67c4564127e3639e04cc8c6f0d988
macrosf <macros_f@163.com>
2015-08-25 17:59:33 +0800

    Page 201

---
+++ src/agile/java/sis/studentinfo/SessionTest.java
@@ -0,0 +1,22 @@
+package agile.java.sis.studentinfo;
+
+import junit.framework.TestCase;
+
+
+abstract public class SessionTest extends TestCase {
+
+	//TODO
+//	private Session session;
+//	private Date StartDate;
+//
+//	public static final int CREDITS = 3;
+//
+//	@Override
+//	public void setUp() {
+//		startDate = createDate(2003, 1, 6);
+//		session = createSession("ENGL", "101", startDate);
+//		session.setNumberOfCredits(CREDITS);
+//	}
+//
+//	abstract
+}
