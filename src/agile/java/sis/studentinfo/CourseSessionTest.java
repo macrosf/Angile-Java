c5da29715dfc2fbec156dc8bec0e65b6838e7570
macrosf <macros_f@163.com>
2015-08-26 17:57:57 +0800

    page 205

--- src/agile/java/sis/studentinfo/CourseSessionTest.java
+++ src/agile/java/sis/studentinfo/CourseSessionTest.java
@@ -2,78 +2,107 @@ package agile.java.sis.studentinfo;
 
 import java.util.Date;
 
-import junit.framework.TestCase;
 import agile.java.util.DateUtil;
 
-public class CourseSessionTest extends TestCase {
 
-	private static final int CREDITS = 3;
-	private CourseSession session;
-
-	private CourseSession createCourseSession() {
-		//return new CourseSession("English", "102", DateUtil.createDate(2003,1,6));
-		CourseSession session = CourseSession.create("English", "102", DateUtil.createDate(2003,1,6));
-		session.setNumberOfCredits(CREDITS);
-		return session;
-	}
-
-	public void setUp() {
-		//session = new CourseSession("English", "101", DateUtil.createDate(2003,1,6));
-		session = createCourseSession();
-	}
-
-//	public void testRosterReport() {
-//		session.enroll(new Student("A"));
-//		session.enroll(new Student("B"));
+//public class CourseSessionTest extends TestCase {
+//page 203
+//	private static final int CREDITS = 3;
+//	private CourseSession session;
 //
-//		String rosterReport = session.getRosterReport();
-//
-//		assertEquals(
-//				RosterReporter.ROSTER_REPORT_HEADER  +
-//				"A\r\nB\r\n" +
-//				RosterReporter.ROSTER_REPORT_FOOTER + "2", rosterReport);
+//	private CourseSession createCourseSession() {
+//		//return new CourseSession("English", "102", DateUtil.createDate(2003,1,6));
+//		CourseSession session = CourseSession.create("English", "102", DateUtil.createDate(2003,1,6));
+//		session.setNumberOfCredits(CREDITS);
+//		return session;
 //	}
-
-	public void testCourseDates() {
-		Date sixteenWeeksOut = DateUtil.createDate(2003, 4, 25);
-		assertEquals(sixteenWeeksOut, session.getEndDate());
-	}
-
-	public void testCount() {
-		//CourseSession.count = 0;
-		CourseSession.resetCount();
-		createCourseSession();
-		//assertEquals(1, CourseSession.count);
-		assertEquals(1, CourseSession.getCount());
-
-		createCourseSession();
-		//assertEquals(2, CourseSession.count);
-		assertEquals(2, CourseSession.getCount());
-	}
-
-	public void testEnrollStudents() {
-		Student student1 = new Student("Mike");
-		session.enroll(student1);
-		assertEquals(CREDITS, student1.getCredits());
-
-		Student student2 = new Student("Janny");
-		session.enroll(student2);
-		assertEquals(CREDITS, student2.getCredits());
-	}
-
-	public void testStudentStatus() {
-		Student student1 = new Student("Mike");
-		session.enroll(student1);
-		assertEquals(CREDITS, student1.getCredits());
-
-		student1.addCredits(9);
-		assertEquals(Student.CREDITS_REQUIRED_FOR_FULL_TIME, student1.getCredits());
-		assertTrue(student1.isFullTime());
-	}
-
+//
+//	public void setUp() {
+//		//session = new CourseSession("English", "101", DateUtil.createDate(2003,1,6));
+//		session = createCourseSession();
+//	}
+//
+////	public void testRosterReport() {
+////		session.enroll(new Student("A"));
+////		session.enroll(new Student("B"));
+////
+////		String rosterReport = session.getRosterReport();
+////
+////		assertEquals(
+////				RosterReporter.ROSTER_REPORT_HEADER  +
+////				"A\r\nB\r\n" +
+////				RosterReporter.ROSTER_REPORT_FOOTER + "2", rosterReport);
+////	}
+//
+//	public void testCourseDates() {
+//		Date sixteenWeeksOut = DateUtil.createDate(2003, 4, 25);
+//		assertEquals(sixteenWeeksOut, session.getEndDate());
+//	}
+//
+//	public void testCount() {
+//		//CourseSession.count = 0;
+//		CourseSession.resetCount();
+//		createCourseSession();
+//		//assertEquals(1, CourseSession.count);
+//		assertEquals(1, CourseSession.getCount());
+//
+//		createCourseSession();
+//		//assertEquals(2, CourseSession.count);
+//		assertEquals(2, CourseSession.getCount());
+//	}
+//
+//	public void testEnrollStudents() {
+//		Student student1 = new Student("Mike");
+//		session.enroll(student1);
+//		assertEquals(CREDITS, student1.getCredits());
+//
+//		Student student2 = new Student("Janny");
+//		session.enroll(student2);
+//		assertEquals(CREDITS, student2.getCredits());
+//	}
+//
+//	public void testStudentStatus() {
+//		Student student1 = new Student("Mike");
+//		session.enroll(student1);
+//		assertEquals(CREDITS, student1.getCredits());
+//
+//		student1.addCredits(9);
+//		assertEquals(Student.CREDITS_REQUIRED_FOR_FULL_TIME, student1.getCredits());
+//		assertTrue(student1.isFullTime());
+//	}
+//
 //	public void testInState() {
 //		Student student = new Student("Frank");
 //		assertFalse(student.isInState());
 //		student
 //	}
+//}
+
+//page 203
+public class CourseSessionTest extends SessionTest {
+
+	public void testCourseDates() {
+
+		Date startDate = DateUtil.createDate(2003, 1, 6);
+		Session session = createSession("ENGL", "200", startDate);
+		Date sixteenWeeksOut = DateUtil.createDate(2003, 4, 25);
+
+		assertEquals(sixteenWeeksOut, session.getEndDate());
+	}
+
+	public void testCount() {
+		CourseSession.resetCount();
+		createSession("", "", new Date());
+		assertEquals(1, CourseSession.getCount());
+
+		createSession("", "", new Date());
+		assertEquals(2, CourseSession.getCount());
+	}
+
+	@Override
+	protected Session createSession(String department, String number,
+			Date startDate) {
+
+		return CourseSession.create(department, number, startDate);
+	}
 }
---
+++ src/agile/java/sis/studentinfo/Session.java
@@ -0,0 +1,141 @@
+package agile.java.sis.studentinfo;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+//page 205
+abstract class Session implements Comparable<Session>{
+
+	private String department;
+	private String number;
+	private int numberOfStudents=0;
+	private Date startDate;
+
+	private List<Student> students = new ArrayList<Student>();
+	private static int count;
+	private int numberOfCredits;
+
+//	public static Session create(String department, String number, Date startDate) {
+//		Session.increasementCount();
+//		return new Session(department, number, startDate);
+//	}
+
+	protected Session(String department, String number, Date startDate) {
+		this.department = department;
+		this.number = number;
+		this.startDate = startDate;
+	}
+
+	//page 205 add
+	abstract protected int getSessionLength();
+
+	private static void increasementCount() {
+		count++;
+	}
+
+	static void resetCount() {
+		count = 0;
+	}
+
+	static int getCount() {
+		return count;
+	}
+
+	public int getNumberOfStudents() {
+		return numberOfStudents;
+	}
+
+	public void enroll(Student student) {
+		numberOfStudents++;
+		student.addCredits(numberOfCredits);
+		students.add(student);
+	}
+
+//	public String getRosterReport() {
+//		StringBuilder buffer = new StringBuilder();
+//
+//		buffer.append(RosterReporter.ROSTER_REPORT_HEADER);
+////		buffer.append(students.get(0).getName()).append(NEW_LINE);
+////		buffer.append(students.get(1).getName()).append(NEW_LINE);
+//		for (Student student : students)
+//			buffer.append(student.getName()).append(RosterReporter.NEW_LINE);
+//		buffer.append(RosterReporter.ROSTER_REPORT_FOOTER);
+//		buffer.append(students.size());
+//
+//		return buffer.toString();
+//	}
+
+	public Date getEndDate() {
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(getStartDate());
+		//final int sessionLength = 16;
+		final int daysInWeek = 7;
+		final int daysFromFridayToMonday = 3;
+		int numberOfDays =
+				getSessionLength() * daysInWeek - daysFromFridayToMonday;
+		calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+		return calendar.getTime();
+	}
+
+	public List<Student> getAllStudents() {
+		return students;
+	}
+
+	/**
+	 * @return the department
+	 */
+	public String getDepartment() {
+		return department;
+	}
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	/**
+	 * @return the number
+	 */
+	public String getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	/**
+	 * @return the numberOfCredits
+	 */
+	public int getNumberOfCredits() {
+		return numberOfCredits;
+	}
+
+	/**
+	 * @param numberOfCredits the numberOfCredits to set
+	 */
+	public void setNumberOfCredits(int numberOfCredits) {
+		this.numberOfCredits = numberOfCredits;
+	}
+
+	@Override
+	public int compareTo(Session that) {
+		int compare = this.getDepartment().compareTo(that.getDepartment());
+		if (compare == 0)
+			compare = this.getNumber().compareTo(that.getNumber());
+		return compare;
+	}
+
+	protected Date getStartDate() {
+		return startDate;
+	}
+
+//	protected int getSessionLength() {
+//		return 16;
+//	}
+}
