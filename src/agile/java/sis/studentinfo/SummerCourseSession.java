80582313f74013cce32b756463d3d56705b28450
macrosf@home <macros_f@163.com>
2015-08-26 23:49:43 +0800

    page 206(add file

---
+++ src/agile/java/sis/studentinfo/SummerCourseSession.java
@@ -0,0 +1,24 @@
+package agile.java.sis.studentinfo;
+
+import java.util.Date;
+
+public class SummerCourseSession extends Session {
+	public static SummerCourseSession create(
+			String department,
+			String number,
+			Date startDate) {
+		return new SummerCourseSession(department, number, startDate);
+	}
+	
+	private SummerCourseSession(
+			String department,
+			String number,
+			Date startDate) {
+		super(department, number, startDate);
+	}
+	@Override
+	protected int getSessionLength() {
+		return 8;
+	}
+
+}
